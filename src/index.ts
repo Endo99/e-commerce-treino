542bff0f96191771a373bdf6a86673bde30b6e86
Endo99 <wellington.endo@edu.unifil.br>
2025-04-30 19:34:37 -0300

    Antes de ter o Router e aprendendo sobre mais sobre o CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express = require("express");
-import { Request, Response } from "express";
+import { Request, Response } from "express"; 
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Bem vindo ao curso de Node.js - tsc-watch");
@@ -9,17 +11,65 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", (req: Request, res: Response) => {
-    let usuarios = [{
-        nome: "Derci Santos",
-        idade: 33
-    },
-    {
-        nome: "João da Silva",
-        idade: 44
-    }]
-
+    
     res.send(usuarios);
 });
+
+type User = {
+    id: number;
+    nome: string;
+    email: string;
+}
+
+let id = 0;
+let usuarios: User[] = [];
+
+app.post("/users", (req: Request, res: Response) =>
+{
+    let user = req.body;
+
+    user.id = ++id;
+    usuarios.push(user);
+    res.send({
+        message: "Usuário criado com sucesso!"
+    })
+
+})
+
+app.get("/users/:id", (req: Request, res: Response) => 
+{
+    let userId = Number(req.params.id);
+
+    let user = usuarios.find(user => user.id === userId);
+
+    res.send(user);
+});
+
+app.put("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let user = req.body;
+
+    let indexOf = usuarios.findIndex((_user: User) => _user.id === userId);
+
+    usuarios[indexOf].nome = user.nome;
+    usuarios[indexOf].email = user.email;
+
+    res.send({
+        message: "Usuário alterado com sucesso!"
+    })
+})
+
+app.delete("/users/:id", (res: Response, req: Request) => {
+    let userId = Number(req.params.id);
+
+    let userFind = usuarios.findIndex((_user: User) => _user.id === userId);
+
+    usuarios.splice(userFind, 1);
+
+    res.send({
+        message: "Usário excluído com sucesso!"
+    })
+})
 
 app.listen(3000, () =>  {
     console.log("Servidor ativo na porta 3000");
