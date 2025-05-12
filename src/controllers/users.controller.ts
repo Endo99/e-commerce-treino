8e496363613d5511824800567228c704ddba14a3
Endo99 <wellington.endo@edu.unifil.br>
2025-05-12 15:03:16 -0300

    Middlware Parte 1

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,4 +1,5 @@
-import { Response, Request } from "express";
+import { Response, Request, NextFunction } from "express";
+//import { messaging } from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore"
 
 type User = {
@@ -9,67 +10,84 @@ type User = {
 
 
 export class UsersController {
-    static async getAll(req: Request, rest: Response) {
+    static async getAll(req: Request, res: Response, next: NextFunction) {
 
-        const snapshot = await getFirestore().collection("users").get();
+        try {
+            const snapshot = await getFirestore().collection("users").get();
+            throw new Error("");
+            const users = snapshot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data()
 
-        const users = snapshot.docs.map(doc =>  {
-            
-            return {
+                };
+            });
+            res.send(users);
+        } catch (error) {
+            next(error);
+        }
+
+    }
+
+    static async getById(req: Request, res: Response) {
+        try {
+            let userId = req.params.id;
+
+            const doc = await getFirestore().collection("users").doc(userId).get();
+
+            res.send({
                 id: doc.id,
                 ...doc.data()
+            });
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro Interno do Servidor"
+            });
+        }
 
-            };
-        });
-
-        rest.send(users)
     }
 
-    static async getById(req: Request, res: Response)
-    {
+    static async save(req: Request, res: Response) {
+        try {
+            let user = req.body;
+
+            const userSalvo = await getFirestore().collection("users").add(user);
+
+            res.status(201).end(`Usuário ${userSalvo.id} criado com sucesso!`)
+        } catch (erro) {
+            res.status(500).send({
+                message: "Erro Interno do Servidor"
+            });
+        }
+
+    }
+
+    static updateById(req: Request, res: Response) {
+
+        try {
+            let userId = req.params.id;
+            let user = req.body as User;
+
+            getFirestore().collection("users").doc(userId).set({
+                nome: user.nome,
+                email: user.email
+            });
+
+            res.send({
+                message: "Usuário alterado com sucesso!"
+            });
+        } catch (error) {
+            res.status(500).send(({
+                message: "Erro Interno do Servidor",
+            }));
+        };
+    }
+
+    static async deleteById(req: Request, res: Response) {
         let userId = req.params.id;
-    
-        const doc = await getFirestore().collection("users").doc(userId).get();
 
-        res.send({
-            id: doc.id,
-            ...doc.data()
-        });
-
-
-    }
-
-    static async save(req: Request, res: Response)
-    {
-        let user = req.body;
-        
-        const userSalvo = await getFirestore().collection("users").add(user);
-
-        res.status(201).end(`Usuário ${userSalvo.id} criado com sucesso!`)
-    
-    }
-
-    static updateById(req: Request, res: Response)
-    {
-        let userId = req.params.id;
-        let user = req.body as User;
-    
-        getFirestore().collection("users").doc(userId).set({
-            nome: user.nome,
-            email: user.email
-        })
-    
-        res.send({
-            message: "Usuário alterado com sucesso!"
-        })
-    }
-
-    static async deleteById(req: Request, res: Response)
-    {
-        let userId = req.params.id;
-    
         await getFirestore().collection("users").doc(userId).delete()
-    
+
         res.status(204).end()
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express = require("express");
 import {routes} from "./routes/index";
 import { initializeApp } from 'firebase-admin/app';
+import { NextFunction, Request, Response } from "express";
 
 initializeApp()
 
 const app = express();
 
 routes(app);
+
+app.use((error: Error, req: Request, response: Response, next: NextFunction) => {
+    response.status(500).send(
+        {
+            message:"Erro Interno do Servidor"
+        }
+    )
+});
 
 app.listen(3000, () =>  {
     console.log("Servidor ativo na porta 3000");
